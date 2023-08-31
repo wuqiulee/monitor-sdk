b789e2199a56bb8a3217e6595d5ec6dba659c161
wuqiuli <wutongte@163.com>
2023-08-31 21:27:26 +0800

    feat: 上报promsie错误

--- src/lib/jsError.js
+++ src/lib/jsError.js
@@ -29,7 +29,46 @@ export default function jsError() {
           selector: lastEvent ? getSelector(lastEvent.path) : "", // 最后交互事件
         };
       }
-      console.log(event, log, lastEvent);
+      console.log(log);
+    },
+    true
+  );
+
+  // 监听promsie错误
+  window.addEventListener(
+    "unhandledrejection",
+    (event) => {
+      let lastEvent = getLastEvent();
+      let message;
+      let filename;
+      let line = 0;
+      let column = 0;
+      let stack = "";
+      let reason = event.reason;
+      if (typeof reason === "string") {
+        message = reason;
+      } else if (typeof reason === "object") {
+        message = reason.message;
+        if (reason.stack) {
+          let matchResult = reason.stack.match(/at\s+(.+):(\d+):(\d+)/);
+          filename = matchResult[1];
+          line = matchResult[2];
+          column = matchResult[3];
+        }
+
+        stack = getLines(reason.stack);
+      }
+      const log = {
+        kind: "stability", // 监控指标的大类
+        type: "error", // 小类型 这是一个错误
+        errorType: "promiseError", // JS执行错误
+        message, // 报错信息
+        filename, // 哪个文件报错了
+        position: `${line}:${column}`,
+        stack,
+        selector: lastEvent ? getSelector(lastEvent.path) : "", // 最后交互事件
+      };
+      console.log(log);
     },
     true
   );
